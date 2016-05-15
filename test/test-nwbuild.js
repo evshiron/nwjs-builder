2a4da81f071a072f530431aebafca615a812eb43
evshiron <evshiron@gmail.com>
2016-05-16 00:52:33 +0800

    chore(test): make mocha tests

---
+++ test/test-nwbuild.js
@@ -0,0 +1,72 @@
+
+const assert = require('assert');
+
+const cp = require('child_process');
+
+describe('nwb', function() {
+
+    describe('download', function() {
+
+        this.timeout(120000);
+
+        it('should print the stable version and exit with code 0', function(done) {
+
+            cp.exec('./bin/nwb stable', function(err, stdout, stderr) {
+
+                if(err) throw err;
+
+                done();
+
+            });
+
+        });
+
+        it('should download the stable version and exit with code 0', function(done) {
+
+            cp.exec('./bin/nwb download', function(err, stdout, stderr) {
+
+                if(err) throw err;
+
+                done();
+
+            });
+
+        });
+
+    });
+
+    describe('nwbuild -h', function() {
+
+        it('should print help and exit with code 0', function(done) {
+
+            cp.exec('./bin/nwb nwbuild -h', function(err, stdout, stderr) {
+
+                if(err) throw err;
+
+                done();
+
+            });
+
+        });
+
+    });
+
+    describe('nwbuild -r', function() {
+
+        this.timeout(15000);
+
+        it('should launch and exit with code 233', function(done) {
+
+            cp.exec('./bin/nwb nwbuild -r ./test/nwb-test/', function(err, stdout, stderr) {
+
+                if(err && err.code == 233) return done();
+                else if(err) throw err;
+                else throw new Error('ERROR_EXIT_CODE_UNEXPECTED');
+
+            });
+
+        });
+
+    });
+
+});
