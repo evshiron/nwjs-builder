52c208bbbd2eb8887fda7dcfefc9a38d2d4ae6d2
evshiron <evshiron@gmail.com>
2016-05-21 21:11:14 +0800

    chore(test): split tests

--- src/test/test-nwbuild.js
+++ src/test/test-nwbuild.js
@@ -1,39 +1,7 @@
 
-const assert = require('assert');
-
 const cp = require('child_process');
 
 describe('nwb', function() {
-
-    describe('download', function() {
-
-        this.timeout(120000);
-
-        it('should print the stable version and exit with code 0', function(done) {
-
-            cp.exec('node ./bin/nwb.js stable', function(err, stdout, stderr) {
-
-                if(err) throw err;
-
-                done();
-
-            });
-
-        });
-
-        it('should download the stable version and exit with code 0', function(done) {
-
-            cp.exec('node ./bin/nwb.js download', function(err, stdout, stderr) {
-
-                if(err) throw err;
-
-                done();
-
-            });
-
-        });
-
-    });
 
     describe('nwbuild -h', function() {
 
